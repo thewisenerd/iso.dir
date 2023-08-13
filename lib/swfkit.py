6291d5bed251a04f0301f06d3b1cdfbcb53183b5
thewisenerd <thewisenerd@protonmail.com>
2023-08-14 04:23:09 +0530

    swfkit: add documentation

--- lib/swfkit.py
+++ lib/swfkit.py
@@ -47,6 +47,20 @@ def parse_file(mm: mmap.mmap, start: int) -> tuple[SwfKitFile, int]:
 
 
 class SwfKitBase:
+    """
+    a SWFKit packaged exe has four sections;
+    after the end of the sections, the remaining data is some form of an archive.
+    the format is as follows,
+    1. 48 bytes of unknown data, with the first 3 bytes being 'SAF'
+    2. a list of files, each file entry is as follows,
+        1. a null-terminated UTF-16 string, the file name
+        2. a 4-byte integer, the size of the zlib compressed data
+        3. the zlib compressed data
+        4. 8 bytes of unknown data
+
+    The exe, upon start, extracts these files to the folder: %temp%/F7B555EB-BC32-486A-8484-B6BEC8318E87/_extra
+    """
+
     def __init__(self, exe: str):
         if not os.path.isfile(exe):
             raise ValueError(f'exe {exe} is not a file')
