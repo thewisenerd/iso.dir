a037e82f974483839c69f3242a992a9010c48393
thewisenerd <thewisenerd@protonmail.com>
2023-08-14 03:34:12 +0530

    run.sh completeness

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/env bash
+
+if [ ! -d '.venv' ]; then
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip3 install -r requirements.txt
+else
+    source .venv/bin/activate
+fi
+
 gunicorn --bind=127.0.0.1:8003 --access-logfile '-' run:app
